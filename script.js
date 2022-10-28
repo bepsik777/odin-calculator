928a3b1f631eedd98e6e8305f52f77259cc8573a
bepsik777 <potocki.alex@gmail.com>
2022-10-28 15:50:13 +0200

    Add functioanlity that show result on display

--- script.js
+++ script.js
@@ -25,9 +25,29 @@ function operate(operator, num1, num2) {
 		return divide(num1, num2)
 	}
 }
+function operationToNumber() {
+	for (let i = 0; i < operation.length; i++) {  //THIS LOOP CHANGE THE OPERATION ITEMS FROM STRING TO NUMBERS
+		if (operation[i] !== '+' && operation[i] !== '-' && operation[i] !== '/' && operation[i] !== '*') {
+			operation[i] = Number(operation[i])
+		}
+	}
+}
 
 function clearDisplay() {
 	display.textContent = ''
+}
+
+function resetAll() {
+	display.textContent = ''
+	displayValue = [];
+	operation = [];
+	result = '';
+}
+
+function logAll() {
+	console.log(`displayValue is ${displayValue}`)
+	console.log(`operation is ${operation}`)
+	console.log(`result is ${result}`)
 }
 
 
@@ -35,42 +55,63 @@ const display = document.querySelector('.display')
 const digits = document.querySelectorAll('.button.digit')
 const operators = document.querySelectorAll('.button.operator')
 const equalButton = document.querySelector('#equal')
-const arrayOfOperators = []
+const clearButton = document.querySelector('#clear')
+const arrayOfOperators = ['+', '-', '*', '/']
 let displayValue = []
 let operation = []
+let result
 
 
 
 
-
+clearButton.addEventListener('click', resetAll);
 
 digits.forEach((digit) => {
 	digit.addEventListener('click', () => { 			//	ADD AN EVENT LISTENET FUNCTION TO THE DIGITS	
 		for (let i = 0; i < arrayOfOperators.length; i++) {
-			if (display.textContent.includes(arrayOfOperators[i]) === true) {  //CHECKS FOR OPERATOR ON DISPLAY AND RESET DISPLAY
+			if (display.textContent.includes(arrayOfOperators[i])) {  //CHECKS FOR OPERATOR ON DISPLAY AND RESET DISPLAY
 				operation.push(arrayOfOperators[i])
 				display.textContent = ''
 			}
 		}
 		display.textContent += digit.textContent;		//  ADD THE NUMBERS ON BUTTONS TO THE DISPLAY
 		displayValue[0] = display.textContent;			//  PUSH THE ACTUAL CONTENT TO A DISPLAY VALUE ARRAY
-		console.log(`display Value is ${displayValue}`)
 	})
 })
 
-operators.forEach((operator) => {
-	arrayOfOperators.push(operator.textContent)
-})
-
-console.log(arrayOfOperators)
 
 operators.forEach((operator) => {
 	operator.addEventListener('click', () => {
-		operation.push(displayValue[0]);							 		//PUSH THE ACTUAL VALUE TO OPERATION
-		console.log(`operation Value is ${operation}`);	 		
-		displayValue[0] = ''	 		//RESET THE DISPLAY VALUE
-		display.textContent = ''	 		//RESET THE TEXT CONTENT OF DISPLAY
-		display.textContent += operator.textContent			// ADD OPERATOR TO DISPLAY CONTENT
-		
+		operation.push(displayValue[0]);			//PUSH THE ACTUAL VALUE TO OPERATION
+		display.textContent = operator.textContent			// ADD OPERATOR TO DISPLAY CONTENT
+		logAll();
+		//
+		if(operation.length === 3) {
+			operationToNumber()
+			console.log(operation)
+			result = operate(operation[1], operation[0], operation[2]);
+			console.log(result)
+			displayValue = [];		// RESET THE DISPLAY VALUE
+			displayValue.push(result);  
+			operation = [];
+			operation.push(result)
+		}
+		//
 	})
+})
+
+
+equalButton.addEventListener('click', () => {
+	operation.push(displayValue[0]);  
+	operationToNumber();
+	 result = operate(operation[1], operation[0], operation[2])
+	 display.textContent = result;  //SHOW THE RESULT ON THE DISPLAY
+	 displayValue = [];		// RESET THE DISPLAY VALUE
+	 displayValue.push(result);  
+	 operation = []
+	 //
+		console.log(displayValue)
+		console.log(operation)
+		console.log(result)
+	 //
 })
